990c81df724e0e9e91f476a0716b13fbafb3b470
CrucifixxColeone <kcoleoney@gmail.com>
2022-07-06 01:48:29 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
-
 /**
- *  * main - Entry point
- *   *
- *    * Return: nothing, return
+ * main - prints putchar
+ * Return : always 0
  */
 int main(void)
 {
-	_putchar('_');
+        _putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -20,5 +18,3 @@ int main(void)
 
 	return (0);
 }
-
-
