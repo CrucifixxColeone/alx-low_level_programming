aafbe1a28d6738f55fc6b1ac60c282d54432a730
CrucifixxColeone <kcoleoney@gmail.com>
2022-07-07 07:16:40 -0700

    main

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#indef MAIN_H
+#define MAIN_H
+
+#include <studio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+
+#endif /* MAIN_H */
