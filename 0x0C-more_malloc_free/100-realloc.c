7712fed1ae3992d7247cbbee904043f4db57d6ea
CrucifixxColeone <kcoleoney@gmail.com>
2022-07-29 07:01:08 -0700

    fare

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,8 +5,9 @@
 *_realloc - Reallocates a memory block using malloc and free
 *@ptr: A pointer to the memory previously allocated
 *@old_size: The size in bytes of the allocated space for ptr
+*@new_size: The size in bytes for the new memory block
 *
-*Retirn: If new_size == old_size - ptr
+*Return: If new_size == old_size - ptr
 *If new_size == 0 and ptr is not NULL - NULL
 *Otherwise - a pointer to the reallocated memory block
 */
